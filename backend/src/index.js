e2f8b524fe8954204ea2affc3ecc884811a03675
ejuniorg92-jpg <231466548+ejuniorg92-jpg@users.noreply.github.com>
2025-09-10 08:09:56 -0400

    Deployment structure: backend/src/index.js

---
+++ backend/src/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.send('Backend working!'));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
